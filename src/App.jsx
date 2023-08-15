fe6e439b3fa10749aff6500a8e2307f5a6dcbc4a
Pali001 <pali.hundal@outlook.com>
2023-08-15 15:34:07 +1200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    return (
+        <header>
+            <h1>Test</h1>
+        </header>
+    );
+}
+
+export default Header;
